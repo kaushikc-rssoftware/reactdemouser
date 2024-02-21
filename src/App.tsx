6a60cd143bf6db5ce67c4ac47fd9b29f732335a8
Kaushik Chakraborty (4354) <kaushikc@rssoftware.co.in>
2024-02-21 14:33:45 +0530

    design

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,82 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const items = [
+    {
+      id: 1,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+    {
+      id: 2,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+    {
+      id: 3,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+    {
+      id: 4,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+    {
+      id: 5,
+      src: 'https://images.pexels.com/photos/1037995/pexels-photo-1037995.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      link: 'link here',
+    },
+  ];
   return (
     <>
-    <img src={reactLogo} className="logo react" alt="React logo" />
-    <h1 className='text-x1 font-bold text-red-500'>Hello world from tailwind</h1>
-    </>
+    
+    <div
+        
+        className='bg-black w-full text-white text-center md:text-left'
+      >
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+          <div className='pb-8 m-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+Title            </p>
+            <p className='py-6'>subtitle</p>
+          </div>
+
+          <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-5'>
+            {items.map(({ id, src, link }) => (
+              <div
+                key={id}
+                className='shadow-md shadow-gray-600 rounded-lg overflow-hidden'
+              >
+                <img
+                  src={src}
+                  alt=''
+                  className='rounded-md duration-200 hover:scale-105'
+                />
+                <div className='flex items-center justify-center'>
+                  <button
+                    className='w-1/2 px-6 py-3 m-4 bg-cyan-500 duration-200 hover:bg-cyan-600'
+                    onClick={() => window.open(link, '_blank')}
+                  >
+                    button
+                  </button>
+                  <button
+                    className='w-1/2 px-6 py-3 m-4 bg-pink-500 duration-200 hover:bg-pink-600'
+                    onClick={() => window.open(link, '_blank')}
+                  >
+                    button
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          </div>
+      </div>
+        
+</>
   )
 }
 
